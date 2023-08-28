c80557628ef1c760ecf18d8132eb800995032f17
saireddy <saireddy5050@gmail.com>
2023-08-28 18:20:54 -0500

    addn

---
+++ Reddy/src/UCM/addn.java
@@ -0,0 +1,15 @@
+package UCM;
+
+public class addn {
+	int a = 10, b = 20;
+	static float f1 = 23.57f;	
+	public static void main(String[] args) {
+		addn ref = new addn();
+		ref.addition12();
+	}
+	
+	public void addition12() {
+		int c = a + b ;
+		System.out.println(c);
+	}
+}
